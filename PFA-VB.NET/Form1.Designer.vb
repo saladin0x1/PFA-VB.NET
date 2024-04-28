d91d5ab97028505a30cc077e8abf00218ba7c4b2
Salah-Eddine El Ouali <50548046+saladin0x1@users.noreply.github.com>
2024-04-28 14:08:35 +0100

    Add files via upload

---
+++ PFA-VB.NET/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        GroupBox3 = New GroupBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        Label7 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(32, 34)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(120, 24)
+        Button1.TabIndex = 0
+        Button1.Text = "Enregistrer"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(32, 74)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(120, 24)
+        Button2.TabIndex = 1
+        Button2.Text = "Note Moyenne"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(32, 112)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(120, 24)
+        Button3.TabIndex = 2
+        Button3.Text = "Note Max"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(32, 147)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(120, 24)
+        Button4.TabIndex = 3
+        Button4.Text = "Note Min"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(32, 192)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(120, 24)
+        Button5.TabIndex = 4
+        Button5.Text = "Vider"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Button5)
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Button4)
+        GroupBox1.Controls.Add(Button3)
+        GroupBox1.Location = New Point(576, 32)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 224)
+        GroupBox1.TabIndex = 5
+        GroupBox1.TabStop = False
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(Label4)
+        GroupBox2.Controls.Add(TextBox3)
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Controls.Add(TextBox1)
+        GroupBox2.Location = New Point(72, 32)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(248, 216)
+        GroupBox2.TabIndex = 6
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Infos Etudiant :"
+        GroupBox2.UseWaitCursor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(17, 147)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(33, 15)
+        Label6.TabIndex = 5
+        Label6.Text = "Note"
+        Label6.UseWaitCursor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(17, 112)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(49, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "Prenom"
+        Label5.UseWaitCursor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(16, 64)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(34, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Nom"
+        Label4.UseWaitCursor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(72, 144)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(160, 23)
+        TextBox3.TabIndex = 2
+        TextBox3.UseWaitCursor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(72, 104)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(160, 23)
+        TextBox2.TabIndex = 1
+        TextBox2.UseWaitCursor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(72, 56)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(160, 23)
+        TextBox1.TabIndex = 0
+        TextBox1.UseWaitCursor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(Label3)
+        GroupBox3.Controls.Add(Label2)
+        GroupBox3.Controls.Add(Label1)
+        GroupBox3.Location = New Point(72, 288)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(704, 208)
+        GroupBox3.TabIndex = 7
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Appréciation des notes"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(24, 168)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 15)
+        Label3.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(24, 104)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 15)
+        Label2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(24, 48)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 0
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(72, 259)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(0, 15)
+        Label7.TabIndex = 8
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoScroll = True
+        AutoSize = True
+        ClientSize = New Size(839, 546)
+        Controls.Add(Label7)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = " Ecole"
+        GroupBox1.ResumeLayout(False)
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ PFA-VB.NET/Form1.vb
@@ -0,0 +1,119 @@
+﻿Public Class Form1
+
+    ' Définir les variables pour stocker les notes et les informations des étudiants
+    Private Const MaxEtudiants As Integer = 100 ' Nombre maximum d'étudiants
+    Private notes(MaxEtudiants - 1) As Double
+    Private infosEtudiants(MaxEtudiants - 1)() As String
+    Private nbEtudiants As Integer = 0
+
+    ' Bouton pour ajouter la note de l'étudiant
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Stocker les informations de l'étudiant
+        Dim nom As String = TextBox1.Text
+        Dim prenom As String = TextBox2.Text
+        Dim note As Double
+
+        ' Assigner directement la valeur du TextBox3.Text à la variable 'note'
+        If Double.TryParse(TextBox3.Text, note) AndAlso note >= 0 AndAlso note <= 20 Then
+            ' Ajouter les informations de l'étudiant s'il y a de la place disponible
+            If nbEtudiants < MaxEtudiants Then
+                notes(nbEtudiants) = note
+                infosEtudiants(nbEtudiants) = {nom, prenom}
+
+                nbEtudiants += 1
+                Label7.Text = nbEtudiants.ToString() & " étudiant(s) sont enregistré(s)."
+            Else
+                MessageBox.Show("Le nombre maximum d'étudiants est atteint.", "Erreur")
+            End If
+        Else
+            MessageBox.Show("Veuillez saisir une note valide entre 0 et 20.", "Erreur de saisie")
+        End If
+    End Sub
+
+    ' Bouton pour calculer la moyenne des notes
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Calculer la moyenne des notes
+        If nbEtudiants > 0 Then
+            Dim somme As Double = 0
+            For i As Integer = 0 To nbEtudiants - 1
+                somme += notes(i)
+            Next
+            Dim moyenne As Double = somme / nbEtudiants
+            Label1.Text = "La moyenne des notes est : " & moyenne.ToString()
+        Else
+            Label1.Text = "Aucune note enregistrée."
+        End If
+    End Sub
+
+    ' Bouton pour trouver la note maximale
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Trouver la note maximale
+        If nbEtudiants > 0 Then
+            Dim noteMax As Double = notes(0)
+            Dim index As Integer = 0
+            For i As Integer = 1 To nbEtudiants - 1
+                If notes(i) > noteMax Then
+                    noteMax = notes(i)
+                    index = i
+                End If
+            Next
+            Dim infos() As String = infosEtudiants(index)
+            Label2.Text = "La note maximale est : " & noteMax.ToString() & " (obtenue par " & infos(0) & " " & infos(1) & ")"
+        Else
+            Label2.Text = "Aucune note enregistrée."
+        End If
+    End Sub
+
+    ' Bouton pour trouver la note minimale
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' Trouver la note minimale
+        If nbEtudiants > 0 Then
+            Dim noteMin As Double = notes(0)
+            Dim index As Integer = 0
+            For i As Integer = 1 To nbEtudiants - 1
+                If notes(i) < noteMin Then
+                    noteMin = notes(i)
+                    index = i
+                End If
+            Next
+            Dim infos() As String = infosEtudiants(index)
+            Label3.Text = "La note minimale est : " & noteMin.ToString() & " (obtenue par " & infos(0) & " " & infos(1) & ")"
+        Else
+            Label3.Text = "Aucune note enregistrée."
+        End If
+    End Sub
+
+    ' Bouton pour effacer la liste des notes
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Réinitialiser le nombre d'étudiants à 0
+        nbEtudiants = 0
+        Label7.Text = "0 étudiant(s) sont enregistré(s)."
+        Label1.Text = " "
+        Label2.Text = " "
+        Label3.Text = " "
+    End Sub
+
+    ' Événement Load du formulaire
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialiser les tableaux
+        For i As Integer = 0 To MaxEtudiants - 1
+            infosEtudiants(i) = New String(1) {}
+        Next
+    End Sub
+
+    ' Événement KeyPress pour TextBox3 pour valider la saisie
+    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+        ' Autoriser uniquement les caractères numériques et le point décimal
+        If Not (Char.IsDigit(e.KeyChar) OrElse e.KeyChar = "." OrElse e.KeyChar = Convert.ToChar(Keys.Back)) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    ' Événement KeyPress pour TextBox1 (Nom) et TextBox2 (Prénom)
+    Private Sub TextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress, TextBox2.KeyPress
+        ' Autoriser uniquement les lettres, les espaces, les tirets et les apostrophes
+        If Not (Char.IsLetter(e.KeyChar) OrElse e.KeyChar = " "c OrElse e.KeyChar = "-"c OrElse e.KeyChar = "'"c OrElse e.KeyChar = Convert.ToChar(Keys.Back)) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
